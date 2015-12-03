mkulke/kubernetes

Add non-resource and API group support to ABAC authorizer, version ABAC policy rules

pkg/auth/authorizer/abac/api/latest/latest.go
@@ -1,0 +1,24 @@
+/*
+Copyright 2014 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package latest
+
+import (
+	"k8s.io/kubernetes/pkg/auth/authorizer/abac/api/v1"
+)
+
+// Codec is the default codec for serializing input that should use the latest supported version.
+var Codec = v1.Codec

caesarxuchao/api

Kubernetes-commit: 2321651518458f9939b6c58030cc44c77ebe0976

pkg/apis/abac/latest/latest.go
@@ -1,0 +1,24 @@
+/*
+Copyright 2014 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package latest
+
+import (
+	"k8s.io/kubernetes/pkg/apis/abac/v1beta1"
+)
+
+// Codec is the default codec for serializing input that should use the latest supported version.
+var Codec = v1beta1.Codec